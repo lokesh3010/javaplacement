f6692cab7b399d3cf70ac30763b8829e91478fe6
lokesh <lokeshramachandran2008@gmail.com>
2026-08-22 06:01:59 +0530

    palinndrome

---
+++ Palindrome.java
@@ -0,0 +1,26 @@
+package javaplacement;
+
+public public class Palindrome {
+    public static void main(String[] args) {
+
+        int number = 121;
+        int originalNumber = number;
+
+        int reverse = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+
+            reverse = reverse * 10 + digit;
+
+            number = number / 10;
+        }
+
+        if (originalNumber == reverse) {
+            System.out.println("Palindrome");
+        }
+        else {
+            System.out.println("Not Palindrome");
+        }
+    }
+} 
